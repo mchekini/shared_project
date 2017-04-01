7815c60ebfe3a862ef3d579f3fa3c3f3d3907afd
sam <s_benbelkacem@esi.dz>
2017-04-01 18:12:48 +0200

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Louzagh A {{name}}</h1>`,
+  template: `<h1> frere poirier {{name}}</h1>`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { name = 'Ahlil'; }
